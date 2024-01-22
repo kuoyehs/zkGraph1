1147f1bef016f70777be27d90734502c9ac78186
KY <kuoyehs@gmail.com>
2024-01-22 10:57:27 +0800

    modify esig

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,8 +4,8 @@ import { Bytes, Event, Block, ByteArray } from "@hyperoracle/zkgraph-lib";
 var null_address = Bytes.fromHexString(
   "0x0000000000000000000000000000000000000000"
   );
-var transfer_event = Bytes.fromHexString(
-  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+var transfer_event_erc721 = Bytes.fromHexString(
+  "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62"
 );
 
 var free = Bytes.fromHexString("Value: 0");
@@ -17,11 +17,11 @@ export function handleBlocks(blocks: Block[]): Bytes {
   // Check if value is 0 to see if it is a free mint
   let free_mint_collections: ByteArray = ByteArray.empty();
   for (let i = events.length - 1; i >= 0; i--) {
-    let flagtopic4 = 'topic4' in events[i]; 
+    //let flagtopic4 = 'topic4' in events[i]; 
     if (
-      flagtopic4 &&
+      //flagtopic4 &&
       events[i].topic2 == null_address && 
-      events[i].esig == transfer_event && 
+      events[i].esig == transfer_event_erc721 && 
       events[i].data == free
       ) {
         free_mint_collections = free_mint_collections.concat(events[i].address);
